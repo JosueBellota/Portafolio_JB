b91bc52bd789716041a644c274a3e7e685929a38
Josue Bellota Ichaso <69655184+JosueBellota@users.noreply.github.com>
2025-12-15 00:14:43 +0100

    animacion de particulas

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
 import './css/Home.css';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
@@ -6,8 +8,90 @@ import GitHubIcon from '@mui/icons-material/GitHub';
 import user from '../img/test_background.jpg';
 
 function Home() {
+  const particlesInit = useCallback(async engine => {
+    await loadFull(engine);
+  }, []);
+
   return (
     <div className="home-container">
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        options={{
+          fullScreen: { enable: false },
+          background: {
+            color: {
+              value: "transparent",
+            },
+          },
+          fpsLimit: 120,
+          interactivity: {
+            events: {
+              onClick: {
+                enable: true,
+                mode: "push",
+              },
+              onHover: {
+                enable: true,
+                mode: "repulse",
+              },
+              resize: true,
+            },
+            modes: {
+              push: {
+                quantity: 4,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.4,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: "#ffffff",
+            },
+            links: {
+              color: "#ffffff",
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 1,
+            },
+            collisions: {
+              enable: true,
+            },
+            move: {
+              directions: "none",
+              enable: true,
+              outModes: {
+                default: "bounce",
+              },
+              random: false,
+              speed: 2,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                area: 800,
+              },
+              value: 80,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              value: { min: 1, max: 5 },
+            },
+          },
+          detectRetina: true,
+        }}
+        className="particles-bg"
+      />
       <div className='home-profile'>
         <div className="hero-image">
             <img src={user} alt="Josue Bellota" />
