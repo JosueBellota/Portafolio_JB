3648bae5a2f5c6d24f386c4951f89628863ee5c3
Josue Bellota Ichaso <69655184+JosueBellota@users.noreply.github.com>
2026-04-20 16:44:25 +0200

    added aditional hosting names to the website

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -3,6 +3,7 @@ import './css/aboutus.css';
 import laptopabout from "../img/desktop.webp";
 import chatgpt from "../img/chatgpt.png";
 import gemini from "../img/gemini.png";
+import reactIcon from "../img/react.png";
 import JavascriptIcon from '@mui/icons-material/Javascript';
 import AndroidIcon from '@mui/icons-material/Android';
 import StorageIcon from '@mui/icons-material/Storage';
@@ -45,7 +46,7 @@ function AboutUs() {
                             <Tooltip title="HTML5" arrow><li ><HtmlIcon sx={{ fontSize: 40, color: "#E34F26" }} /></li></Tooltip>
                             <Tooltip title="CSS3" arrow><li ><CssIcon sx={{ fontSize: 40, color: "#1572B6" }} /></li></Tooltip>
                             <Tooltip title="JavaScript" arrow><li ><JavascriptIcon sx={{ fontSize: 50, color: "black", backgroundColor: "#f7df1e", borderRadius: "50%" }} /></li></Tooltip>
-                            <Tooltip title="React" arrow><li ><TerminalIcon sx={{ fontSize: 35, color: "#61DAFB" }} /></li></Tooltip>
+                            <Tooltip title="React" arrow><li ><img src={reactIcon} alt="React" style={{ width: '35px', height: '35px', objectFit: 'contain' }} /></li></Tooltip>
                             <Tooltip title="PHP" arrow><li ><CodeIcon sx={{ fontSize: 35, color: "#4F5D95" }} /></li></Tooltip>
                             <Tooltip title="Android" arrow><li ><AndroidIcon sx={{ fontSize: 35, color: "#3DDC84" }} /></li></Tooltip>
                             <Tooltip title="SQL/Databases" arrow><li ><StorageIcon sx={{ fontSize: 35, color: "#00758F" }} /></li></Tooltip>
